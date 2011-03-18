a2d55f8e2c6bc6ff22ea55eb94f5ad34f14d4362
Bram Bonné <bram.bonne@gmail.com>
2011-03-18 11:52:58 +0100

    Disable session hijacking protection by default.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 pref("extensions.nofix.sessidonly", true);
 pref("extensions.nofix.logtofile", false);
-pref("extensions.nofix.preventhijacking", true);
+pref("extensions.nofix.preventhijacking", false);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.nofix@cs.kuleuven.be.description", "chrome://nofix/locale/overlay.properties");
